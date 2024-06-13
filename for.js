c2110add2ba53987096459831b79099ef9cee965
Muugii9902 <muugii9902@gmail.com>
2024-06-13 11:26:25 +0800

    js_loop

--- for.js
+++ for.js
@@ -19,92 +19,49 @@ for (let i = 1; i <= 100; i++) {
 }
 console.log("1-100 niilber", sum);
 // bodlogo 5
+let n = 10;
 let sum1 = 0;
-let too1 = 0;
-for (let i = 1; i <= 101; i++) {
-  too1 = too1 + 1;
-  sum1 = sum1 + too1;
-}
-console.log("1-100 niilber", sum1);
-// bodlogo 6
-let sum2 = 1;
-let too2 = 2;
-
-let num1 = 4;
-for (let i = 0; i <= 4; i++) {
-  sum2 = sum2 * too2 * num * num1;
-}
-console.log("1-100 niilber", sum2);
-// 5 Write a program to print natural numbers up to N and find their sum
-sum = 0;
-let n = 4;
 for (let i = 1; i <= n; i++) {
-  sum += i;
+  sum1 += n;
 }
-console.log("The sum of the natural numbers is:", sum);
-
-// 6 Write a program to find the factorial of N numbers use wile loop
-let fact = 1;
-while (n > 1) {
-  fact *= n;
-  n--;
+console.log("n-toonii niilber", sum1);
+//bodlogo 6
+let n1 = 5;
+let urjver = 1;
+for (let i = 1; i <= n1; i++) {
+  urjver *= i;
 }
-console.log("The factorial is:", fact);
-
-// 7 Find the sum of the all digits in the given number
-sum = 0;
-let num = 1234567;
-while (num > 0) {
-  let digit = num % 10; // Get the last digit of the number.
-  sum += digit; // Add it to the sum.
-  num = (num - digit) / 10; // Remove the last digit from the number.
+console.log("n hurtleh toonii urjver", urjver);
+//bodlogo 7
+let number = 123;
+let digit = 0;
+let dum = 0;
+while (number !== 0) {
+  digit = number % 10;
+  number = number - digit;
+  number = number / 10;
+  dum = dum + digit;
 }
-console.log("The sum of the all digits is:", sum);
-
-// 8 Find the sum of the even digits in the given number
-sum = 0;
-num = 1234567;
-while (num > 0) {
-  let digit = num % 10; // Get the last digit of the number.
-  if (digit % 2 === 0) {
-    // Check if the digit is even.
-    sum += digit; // Add it to the sum.
+console.log("digit dum", dum);
+// bodlogo 8
+let number1 = 1234;
+let digit1 = 0;
+let dum1 = 0;
+while (number !== 0) {
+  digit1 = number1 % 10;
+  number1 = number1 - digit1;
+  number1 = number1 / 10;
+  dum = dum + digit1;
+  if (digit1 % 2 === 0) {
+    dum1 = dum1 + digit1;
   }
-  num = (num - digit) / 10; // Remove the last digit from the number.
 }
-console.log("The sum of the even digits is:", sum);
-
-// 9 Write a program to print odd numbers up to N and find their sum
-n = 89;
-sum = 0;
-while (n > 0) {
-  if (n % 2 !== 0) {
-    // Check if n is odd.
-    console.log(n); // Print the odd number.
-    sum += n; // Add it to the sum.
-  }
-  n--;
+console.log("even digit", dum1);
+//bodlogo 9
+let n2 = 28;
+let sondgoitoo = 0;
+for (let i = 1; i <= n2; i += 2) {
+  console.log("sondgoi-too", i);
+  sondgoitoo = sondgoitoo + n2;
 }
-console.log("The sum of the odd digits is:", sum);
-// 10 Write a program to print even numbers up to N and find their sum
-n = 89;
-sum = 0;
-while (n > 0) {
-  if (n % 2 === 0) {
-    // Check if n is even.
-    console.log(n); // Print the even number.
-    sum += n; // Add it to the sum.
-  }
-  n--;
-}
-console.log("The sum of the even digits is:", sum);
-// 11 Write a program to print the inverse of the given positive number eg input: 123 output: 321
-let reversed = 0;
-num = 8934;
-let originalNum = num;
-while (num > 0) {
-  let digit = num % 10; // Get the last digit of the number.
-  reversed = reversed * 10 + digit; // Append it to the reversed number.
-  num = (num - digit) / 10; // Remove the last digit from the number.
-}
-console.log("The inverse of", originalNum, "is:", reversed);
+console.log("sondgoitoo", sondgoitoo);
